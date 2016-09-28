c095bed92bd7ab44c0e79794faa7b7f9639c7f58
fftdd <tecfcs@gmail.com>
2016-09-28 21:34:19 +0800

    nodejs http server test

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var http = require('http'),
+    path = require('path');
+
+var server = http.createServer( (req, res)=> {
+    req.url = path.normalize(url.parse(req.url).pathname);
+
+    if (req.url.length > 1 && req.url[req.url.length - 1] === '/')
+	req.url = req.url.substring(0, req.url.length - 1);
+
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+
+    res.end('nodejs server test');
+});
+
+server.listen(80);
